fb3725e9ee531b6743db9524d08e08117ddc19fe
lux-source <andrei-lucian@hotmail.com>
2023-11-20 21:30:19 +0100

    Commit: Implement Employee Time Tracking System
    Summary:
    This commit introduces a significant update.
    
    We've now implemented the foundational structure for the Gavicu_Paga company's employee time tracking system. The update includes defining several key structs (DNI, DailyHours, and Employee) and developing the primary functionality for registering a new employee.
    
    Key Changes:
    1. Added Structs:
       - `DNI` for storing employee's DNI information.
       - `DailyHours` to record the entry and exit times of employees.
       - `Employee` to hold employee data including name, last name, DNI, working hours, and salary details.
    
    2. `main` Function Enhancements:
       - Implemented a user interface with a menu offering options to register new employees, record entry and exit times, calculate monthly salaries, remove employees, and exit the program.
       - Initialized variables for current time tracking.
    
    3. Implemented `Get_time` Function:
       - A utility function provided to fetch the current time.
    
    4. Started `registerEmployee` Function:
       - Began implementing the functionality for registering a new employee. Currently, it prompts for and reads the employee's name.
    
    5. Other:
       - Defined the maximum number of employees (`company[20]`) for the system.
    
    This update lays the groundwork for further development of the employee time tracking system, with subsequent enhancements to include complete registration functionality, time recording, and salary calculations.
    
    Note: The currentEmployees parameter in registerEmployee is a placeholder and will be further utilized in upcoming updates.

---
+++ library.h
@@ -0,0 +1,9 @@
+//
+// Created by andre on 20/11/2023.
+//
+#ifndef LIBRERIA_LIBRARY_H
+#define LIBRERIA_LIBRARY_H
+
+void Get_time(int *hour,int *min);
+
+#endif //LIBRERIA_LIBRARY_H
--- main.c
+++ main.c
@@ -1,12 +1,128 @@
 //
 // Created by andre on 20/11/2023.
 //
+// Group Number 10
+//
+// Members: ALONSO GARDE ALITE, LUCIAN ANDREI NEGOITA, HÉCTOR RUÍZ LÓPEZ
+//
+#include "library.h"
+#include<time.h>
 #include <stdio.h>
+
+// Structs
+
+typedef struct {
+    int numbers[8];
+    char character;
+} DNI;
+
+typedef struct {
+    int hourEntry;
+    int minEntry;
+    int hourExit;
+    int minExit;
+} DailyHours;
+
+typedef struct {
+    char name[20];
+    char lastName[20];
+    int employeeId;
+    int monthlyHours;
+    int yearlyHours;
+    float hourlyRate;
+    DNI dni;
+    DailyHours workedTime[31];
+} Employee;
+
+// Headers
+
+void Get_time(int *hour, int *min);
+
+void registerEmployee(Employee company[], int size, int currentEmployees);
 
 int main() {
 
+    int hour = -1;
+    int min = -1;
+    int option = -1;
+    Get_time(&hour, &min);
 
-    printf("Hello World!");
+    Employee company[20];
+
+    //printf("Time is: %d:%d", hour, min);
+
+    do {
+
+        printf("\n\n1. Register a new employee.");
+        printf("\n2. Record entry time.");
+        printf("\n3. Record exit time.");
+        printf("\n4. Calculate monthly salary.");
+        printf("\n5. Remove an employee.");
+        printf("\n6. Exit the program.");
+
+        printf("\n");
+
+        scanf(" %d", &option);
+
+        switch (option) {
+            case 1:
+                printf("\n");
+                registerEmployee(company,20,5);
+                printf("\n");
+                break;
+            case 2:
+                printf("\n");
+                printf("Getting Entry Time...:");
+                //Get_time();
+                printf("\n");
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                printf("\nBye!");
+                break;
+            default:
+                printf("\n\nTry again, option not valid!");
+        }
+    } while (option != 6);
+
 
     return 0;
 }
+
+
+
+// Given function by Arturo, that provides time
+
+void Get_time(int *hour, int *min) {
+    struct tm *h;
+    time_t hor;
+    hor = time(NULL);
+    h = localtime(&hor);
+    *hour = h->tm_hour;
+    *min = h->tm_min;
+}
+
+// Option 1, Register New Employee
+
+void registerEmployee(Employee company[], int size, int currentEmployees){
+
+    printf("\nEnter name: ");
+    scanf("%s", company->name);
+
+
+}
+
+// Option 2, Record Entry Time
+
+// Option 3, Record Exit Time
+
+// Option 4, Calculate Monthly Salary
+
+// Option 5, Remove An Employee
+
+// Option 6, Exit The Program
